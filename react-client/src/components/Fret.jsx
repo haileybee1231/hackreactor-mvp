e4a306667826cb34193aed16f542e12d990cb5e6
haileybee1231 <haileybee1231@gmail.com>
2018-02-16 15:12:38 -0600

    Adds functioning guitar neck with state

--- react-client/src/components/Fret.jsx
+++ react-client/src/components/Fret.jsx
@@ -9,23 +9,64 @@ class Fret extends React.Component {
     }
   }
 
+  onClick() {
+    if (!this.state.selected) {
+      this.props.selectFret(this.props.fret);
+    } else {
+      this.props.selectFret(0);
+    this.setState({
+        selected: !this.state.selected
+      });
+    }
+  }
 
   render() {
     const styles = {
-      unselectedHoriz: {
-        width: '50px'
+      fret: {
+        position: 'relative',
+        float: 'left',
       },
-      unselectedVert: {
-        width: '50px',
-        marginLeft: '-10px'
+      horizontal: {
+        width: '25px'
+      },
+      vertical: {
+        width: '25px',
+      },
+      dot: {
+        position: 'relative',
+        left: '-20px',
+        top: '-5px',
+        height: '15px',
+        display: 'inline'
       }
     }
 
     if (!this.state.selected) {
       return (
-        <div className="fret">
-          <img style={styles.unselectedHoriz} src='https://image.flaticon.com/icons/svg/53/53916.svg'/>
-          <img style={styles.unselectedVert} src='http://clipground.com/images/vertical-clipart-4.png'/>
+        <div style={styles.fret}>
+          <img
+            style={styles.horizontal}
+            onClick={this.onClick.bind(this)}
+            src='https://image.flaticon.com/icons/svg/53/53916.svg'/>
+          <img
+            style={styles.vertical}
+            src='http://clipground.com/images/vertical-clipart-4.png'/>
+        </div>
+      )
+    } else {
+      return (
+        <div style={styles.fret}>
+          <div onClick={this.onClick.bind(this)}>
+            <img
+              style={styles.horizontal}
+              src='https://image.flaticon.com/icons/svg/53/53916.svg'/>
+            <img
+              style={styles.dot}
+              src='http://www.clker.com/cliparts/y/K/J/U/l/Z/black-dot-md.png'/>
+            <img
+              style={styles.vertical}
+              src='http://clipground.com/images/vertical-clipart-4.png'/>
+          </div>
         </div>
       )
     }
--- react-client/src/components/Guitar.jsx
+++ react-client/src/components/Guitar.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import Fret from '../components/Fret.jsx';
+import String from '../components/String.jsx';
 
 const Guitar = (props) => (
   <div>
     <h4>Guitar</h4>
-    <Fret />
+    {props.chord.map((string, index) => {
+      return (
+        <div style={{'clear':'both'}} key={index}>
+          <String
+            setNote={props.setNote}
+            string={string.string}
+            fret={string.fret}/>
+        </div>
+      )
+    })}
   </div>
 )
 
